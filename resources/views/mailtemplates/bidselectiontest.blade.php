3683f50447fc516ec6fb1039b4eb19c99add571d
EagleAglow <randy@atomicwizard.com>
2021-07-14 18:56:15 -0700

    Version: 14JULY2021 Add note to bid result email

--- resources/views/mailtemplates/bidselectiontest.blade.php
+++ resources/views/mailtemplates/bidselectiontest.blade.php
@@ -2,7 +2,9 @@
 Hello **{{$name}}**,  {{-- use double space for line break or <br> --}}
 You have completed bidding.  Your selection is shown below.  {{-- use double space for line break --}}
 The shift schedule is in the attached "ics" file.  {{-- use double space for line break --}}
-You can also login to view or print the shift schedule.    {{-- use double space for line break --}}
+You can also login to view or print the shift schedule.  {{-- use double space for line break --}}
+Please note that the displayed/attached schedule is static, and  {{-- use double space for line break --}}
+changes such as shift swaps need to be manually adjusted by user.  {{-- use double space for line break --}}
 Regards,  {{-- use double space for line break --}}
 {{$from_name}}  {{-- use double space for line break --}}
 
